bb10b579ef99b78ab0e4182b0412bca967197274
avijit <avijit@ubuntu.ubuntu-domain>
2012-03-31 12:51:13 -0400

    kmercounter working

--- src/main/java/org/avrotest/kmerCounter.java
+++ src/main/java/org/avrotest/kmerCounter.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.StringTokenizer;
+
 import org.avrotest.fastqrecord;
 import org.apache.avro.mapred.AvroCollector;
 import org.apache.avro.mapred.AvroJob;
@@ -14,6 +16,7 @@ import org.apache.avro.mapred.AvroReducer;
 import org.apache.avro.mapred.AvroWrapper;
 import org.apache.avro.mapred.Pair;
 import org.apache.avro.reflect.ReflectData;
+import org.apache.avro.util.Utf8;
 import org.apache.avro.Schema;
 
 import org.apache.commons.cli.CommandLine;
@@ -27,6 +30,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
@@ -39,31 +43,53 @@ import org.apache.hadoop.util.ToolRunner;
 
 
 
+
 public class kmerCounter
 {       
         
-        public static class KmerCounterMapper extends AvroMapper<fastqrecord, fastqrecord>        
+	    public static class KmerCounterMapper extends AvroMapper<fastqrecord, Pair<Utf8, Long>>        
         {
-                private static int K = 0;
+                private static long K = 0;
 
                 public void configure(JobConf job) 
                 {
-                        //K = Integer.parseInt(job.get("K"));
+                       K= job.getLong("K", 0);
+                       //System.out.println("Value of K "+ K);
                         
                 }
-          
- 
-             
+
                 @Override
                 public void map(fastqrecord compressed_read, 
-                    AvroCollector<fastqrecord> output, Reporter reporter)
+                    AvroCollector<Pair<Utf8, Long>> output, Reporter reporter)
                                                 throws IOException {
 
-                	System.out.print("Read = "+compressed_read.read);
-                   	output.collect(compressed_read);
-        }
+                	String id = compressed_read.id.toString();
+                	String seq= compressed_read.read.toString();
+                  //  System.out.println(id+" " + seq);
+
+                	/* We convert every kmer to its canonical for the kmer counting phase so that
+            		 * canonical kmers dont appear at different places
+            		 */
+                	for (int i=0; i<= seq.length() - K; i++)
+                	{
+                		String kmer = seq.substring(i,(int)(i+K));
+                		String kmerCanonical = Node.canonicalseq(kmer);
+                		//
+                		output.collect(new Pair<Utf8,Long>(new Utf8(kmerCanonical),1L));
+                    }
+                }
         }
        
+	    public static class KmerCounterReducer extends AvroReducer<Utf8, Long, Pair<Utf8, Long> > {
+	    	@Override
+	     	public void reduce(Utf8 kmer, Iterable<Long> counts, AvroCollector<Pair<Utf8,Long>> collector,
+	     	Reporter reporter) throws IOException {
+	     	long sum = 0;
+	     	for (long count : counts)
+	     	sum += count;
+	     	collector.collect(new Pair<Utf8,Long>(kmer, sum));
+	     	}
+	     } 
 
   
   protected int run(String inputPath, String outputPath, long K) throws Exception {  
@@ -74,12 +100,14 @@ public class kmerCounter
 
     FileInputFormat.addInputPath(conf, new Path(inputPath));
     FileOutputFormat.setOutputPath(conf, new Path(outputPath));
-
+    conf.setLong("K", K);
     fastqrecord read = new fastqrecord();
     AvroJob.setInputSchema(conf, read.getSchema());
-    AvroJob.setMapOutputSchema(conf, read.getSchema());
+    AvroJob.setOutputSchema(conf, new Pair<Utf8,Long>(new Utf8(""), 0L).getSchema());
+    //AvroJob.setMapOutputSchema(conf, new Pair<Utf8,Long>(new Utf8(""), 0L).getSchema());
     AvroJob.setMapperClass(conf, KmerCounterMapper.class);
-    conf.setNumReduceTasks(0);
+    AvroJob.setReducerClass(conf, KmerCounterReducer.class);
+    //conf.setNumReduceTasks(0);
     // Delete the output directory if it exists already
     Path out_path = new Path(outputPath);
     if (FileSystem.get(conf).exists(out_path)) {
